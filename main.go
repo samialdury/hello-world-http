a100422f9f0a85faf9ef41d84bd45f1fb4c2f13e
Sami Al-Dury <git@aldury.com>
2024-02-23 19:57:34 +0100

    feat: add endpoint

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "log/slog"
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+)
 
 func main() {
-	slog.Info("Hello world")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Incoming request", "method", r.Method, "host", r.Host, "path", r.URL.Path)
+
+		res := Response{
+			RemoteAddr:    r.RemoteAddr,
+			XRealIP:       r.Header.Get("X-Real-Ip"),
+			XForwardedFor: r.Header.Get("X-Forwarded-For"),
+		}
+
+		writeJSON(w, http.StatusOK, res)
+	})
+
+	port := getEnv("PORT", "8080")
+
+	slog.Info("Starting app...", "port", port)
+
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), mux)
+}
+
+type Response struct {
+	RemoteAddr    string `json:"remote_addr"`
+	XRealIP       string `json:"x_real_ip"`
+	XForwardedFor string `json:"x_forwarded_for"`
+}
+
+func getEnv(key, fallback string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = fallback
+	}
+	return value
+}
+
+func writeJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(v)
 }
